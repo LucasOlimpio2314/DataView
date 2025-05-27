a2e1c316e891e4cd73b65b1cc1cd3ed81e46ddda
Lucas Barreto Olimpio <80434166+LucasOlimpio2314@users.noreply.github.com>
2025-05-27 17:57:33 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'serve.test',
-  password: '@Olimpio123773',
+  password: 'SENHA-DO-BANCO',
   database: 'DataView'
 });
 
